75631ce6a78b7cb61fbd8d8fe68a1b15397d4277
Erick Scur <erickscur@gmail.com>
2022-12-21 16:43:38 -0300

    test: ensure JwtAdapter returns a token on success

--- src/infra/criptography/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter.spec.ts
@@ -17,4 +17,20 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
   })
+
+  test('Should return a token on sign success', async () => {
+    const sut = makeSut()
+    const accessToken = await sut.encrypt('any_value')
+    expect(accessToken).toBe('any_token')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
