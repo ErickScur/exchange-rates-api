b3b56033b6e5f6d43fa0eb7c92c629b3e8a7bafe
Erick Scur <erickscur@gmail.com>
2022-12-21 15:28:49 -0300

    refactor: validate required fields

--- src/presentation/controllers/authentication/login/login-controller.ts
+++ src/presentation/controllers/authentication/login/login-controller.ts
@@ -12,12 +12,11 @@ export class LoginController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    if (!password) {
-      return badRequest(new MissingParamError('password'))
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
     await this.authentication.auth({ email, password })
