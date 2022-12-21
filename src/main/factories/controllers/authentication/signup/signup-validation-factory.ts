1a2637239e355b13ccc3238a21b5b82777324b84
Erick Scur <erickscur@gmail.com>
2022-12-21 20:18:43 -0300

    feat: CompareFieldsValidation

--- src/main/factories/controllers/authentication/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/authentication/signup/signup-validation-factory.ts
@@ -1,11 +1,18 @@
 import { Validation } from '../../../../../presentation/protocols/validation'
-import { RequiredFieldValidation } from '../../../../../validation/validators/required-field-validation'
-import { ValidationComposite } from '../../../../../validation/validators/validation-composite'
+import {
+  CompareFieldsValidation,
+  RequiredFieldValidation,
+  ValidationComposite,
+} from '../../../../../validation/validators'
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+
+  validations.push(
+    new CompareFieldsValidation('password', 'passwordConfirmation'),
+  )
   return new ValidationComposite(validations)
 }
