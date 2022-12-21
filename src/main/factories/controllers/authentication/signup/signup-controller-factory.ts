0b16d30cd296c1c39be4e9c03d466bffc100923e
Erick Scur <erickscur@gmail.com>
2022-12-21 19:29:50 -0300

    fix: add BcryptAdapter to signup factory

--- src/main/factories/controllers/authentication/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/authentication/signup/signup-controller-factory.ts
@@ -1,4 +1,5 @@
 import { DbAddAccount } from '../../../../../data/usecases/authentication/add-account/db-add-account'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { PrismaInstance } from '../../../../../infra/db/prisma/helpers/prisma-instance'
 import { AccountPrismaRepository } from '../../../../../infra/db/prisma/repositories/account/account-prisma-repository'
 import { SignUpController } from '../../../../../presentation/controllers/authentication/signup/signup-controller'
@@ -8,9 +9,12 @@ import { makeLogControllerDecorator } from '../../../decorators/log-controller-d
 export const makeSignUpController = (): Controller => {
   const prismaInstance = new PrismaInstance()
   const addAccountRepository = new AccountPrismaRepository(prismaInstance)
+  const salt = 12
+  const bcryptAdapater = new BcryptAdapter(salt)
   const dbAddAccount = new DbAddAccount(
     addAccountRepository,
     addAccountRepository,
+    bcryptAdapater,
   )
   const controller = new SignUpController(dbAddAccount)
   return makeLogControllerDecorator(controller)
