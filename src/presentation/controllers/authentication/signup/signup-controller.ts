78239a5fe99d42e005c4c43f17246d1858acee8c
Erick Scur <erickscur@gmail.com>
2022-12-21 11:43:10 -0300

    feat: success and server error helpers

--- src/presentation/controllers/authentication/signup/signup-controller.ts
+++ src/presentation/controllers/authentication/signup/signup-controller.ts
@@ -1,29 +1,44 @@
+import { ok } from 'assert'
 import { AddAccount } from '../../../../domain/usecases/authentication/add-account'
 import { InvalidParamError, MissingParamError } from '../../../errors'
-import { badRequest } from '../../../helpers/http-helper'
+import { badRequest, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class SignUpController implements Controller {
   constructor(private readonly addAccount: AddAccount) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ]
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation)
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+
+      const account = this.addAccount.add({
+        name,
+        email,
+        password,
+      })
+
+      return {
+        statusCode: 200,
+        body: ok(account),
+      }
+    } catch (error) {
+      return serverError(error)
     }
-
-    const { name, email, password, passwordConfirmation } = httpRequest.body
-
-    if (password !== passwordConfirmation)
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-
-    this.addAccount.add({
-      name,
-      email,
-      password,
-    })
   }
 }
