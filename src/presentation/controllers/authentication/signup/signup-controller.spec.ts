521bd2a81bf5b34d90a9c8ae6d11df78c32b99e5
Erick Scur <erickscur@gmail.com>
2022-12-21 19:45:46 -0300

    test: ensure SignUpController calls validation with correct value

--- src/presentation/controllers/authentication/signup/signup-controller.spec.ts
+++ src/presentation/controllers/authentication/signup/signup-controller.spec.ts
@@ -8,15 +8,33 @@ import {
   InvalidParamError,
   ServerError,
   HttpRequest,
+  Validation,
 } from './signup-controller-protocols'
+
+interface SutTypes {
+  sut: SignUpController
+  addAccountStub: AddAccount
+  validationStub: Validation
+}
 
 const makeSut = (): SutTypes => {
   const addAccountStub = makeAddAccount()
-  const sut = new SignUpController(addAccountStub)
+  const validationStub = makeValidation()
+  const sut = new SignUpController(addAccountStub, validationStub)
   return {
     sut,
     addAccountStub,
+    validationStub,
   }
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
 }
 
 const makeAddAccount = (): AddAccount => {
@@ -33,11 +51,6 @@ const makeAddAccount = (): AddAccount => {
     }
   }
   return new AddAccountStub()
-}
-
-interface SutTypes {
-  sut: SignUpController
-  addAccountStub: AddAccount
 }
 
 const makeFakeRequest = (): HttpRequest => {
@@ -181,5 +194,15 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError(''))
+  })
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+
+    const httpRequest = makeFakeRequest()
+
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
