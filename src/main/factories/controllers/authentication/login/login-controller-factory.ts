0e2e1612015a9fa001c5cc9aff6ca87d058ee716
Erick Scur <erickscur@gmail.com>
2022-12-21 19:36:26 -0300

    feat: add BcryptAdapter to login factory

--- src/main/factories/controllers/authentication/login/login-controller-factory.ts
+++ src/main/factories/controllers/authentication/login/login-controller-factory.ts
@@ -1,4 +1,5 @@
 import { DbAuthentication } from '../../../../../data/usecases/authentication/login/db-authentication'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { PrismaInstance } from '../../../../../infra/db/prisma/helpers/prisma-instance'
 import { AccountPrismaRepository } from '../../../../../infra/db/prisma/repositories/account/account-prisma-repository'
@@ -11,7 +12,13 @@ export const makeLoginController = (): Controller => {
   const prismaInstance = new PrismaInstance()
   const repository = new AccountPrismaRepository(prismaInstance)
   const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  const authentication = new DbAuthentication(repository, jwtAdapter)
+  const salt = 12
+  const bcryptAdapater = new BcryptAdapter(salt)
+  const authentication = new DbAuthentication(
+    repository,
+    jwtAdapter,
+    bcryptAdapater,
+  )
   const loginController = new LoginController(authentication)
   return makeLogControllerDecorator(loginController)
 }
--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt'
 import request from 'supertest'
 import { PrismaInstance } from '../../infra/db/prisma/helpers/prisma-instance'
 import app from '../config/app'
@@ -33,11 +34,13 @@ describe('Auth Routes', () => {
 
   describe('POST /login', () => {
     test('Should return 200 on login', async () => {
+      const password = await hash('valid_password', 12)
+
       await prismaInstance.account.create({
         data: {
           name: 'valid_name',
           email: 'valid_email@mail.mail',
-          password: 'valid_password',
+          password,
         },
       })
 
