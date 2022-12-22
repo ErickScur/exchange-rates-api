d6e4fcc2abd211b8095308223c9fd963e9f98fea
Erick Scur <erickscur@gmail.com>
2022-12-22 11:31:49 -0300

    test: ensure DbVerifyToken calls LoadAccountByIdRepository with correct value

--- src/data/usecases/authentication/verify-token/db-verify-token.spec.ts
+++ src/data/usecases/authentication/verify-token/db-verify-token.spec.ts
@@ -1,10 +1,7 @@
 import { Decrypter } from '../../../protocols/criptography/decrypter'
+import { LoadAccountByIdRepository } from '../../../protocols/db/account/load-account-by-id-repository'
+import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbVerifyToken } from './db-verify-token'
-
-interface SutTypes {
-  sut: DbVerifyToken
-  decrypterStub: Decrypter
-}
 
 const makeDecrypter = (): Decrypter => {
   class DecrypterStub implements Decrypter {
@@ -15,14 +12,42 @@ const makeDecrypter = (): Decrypter => {
   return new DecrypterStub()
 }
 
+const makeLoadAccountById = (): LoadAccountByIdRepository => {
+  class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
+    loadById(id: string): Promise<AccountModel> {
+      const account = makeFakeAccount()
+      return new Promise((resolve) => resolve(account))
+    }
+  }
+  return new LoadAccountByIdRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbVerifyToken
+  decrypterStub: Decrypter
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+}
+
 const makeSut = (): SutTypes => {
   const decrypterStub = makeDecrypter()
-  const sut = new DbVerifyToken(decrypterStub)
+  const loadAccountByIdRepositoryStub = makeLoadAccountById()
+  const sut = new DbVerifyToken(decrypterStub, loadAccountByIdRepositoryStub)
 
   return {
     sut,
     decrypterStub,
+    loadAccountByIdRepositoryStub,
   }
+}
+
+const makeFakeAccount = (): AccountModel => {
+  const account: AccountModel = {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'hashed_password',
+  }
+  return account
 }
 
 describe('DbVerifyToken UseCase', () => {
@@ -40,5 +65,16 @@ describe('DbVerifyToken UseCase', () => {
 
     const response = await sut.verify('any_token')
     expect(response).toBe(null)
+  })
+
+  test('Should call LoadAccountByIdRepository with correct value', async () => {
+    const { sut, decrypterStub, loadAccountByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadAccountByIdRepositoryStub, 'loadById')
+
+    jest
+      .spyOn(decrypterStub, 'decrypt')
+      .mockReturnValueOnce(new Promise((resolve) => resolve('valid_id')))
+    await sut.verify('any_token')
+    expect(loadByIdSpy).toHaveBeenCalledWith('valid_id')
   })
 })
