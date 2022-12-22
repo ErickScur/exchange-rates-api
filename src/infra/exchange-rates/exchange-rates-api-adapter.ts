4f30e1443359f7ea20db61c6256685c062a4357b
Erick Scur <erickscur@gmail.com>
2022-12-22 19:25:37 -0300

    feat: exchange rates api adapter

---
+++ src/infra/exchange-rates/exchange-rates-api-adapter.ts
@@ -0,0 +1,34 @@
+import { GetCurrenciesExchangeRate } from '../../data/protocols/exchange-rates/get-currencies-exchange-rate'
+import {
+  GetCurrenciesExchangeRateModel,
+  ExchangeRateModel,
+} from '../../domain/models/exchange-rate/exchange-rate'
+import axios from 'axios'
+
+export class ExchangeRatesApiAdapter implements GetCurrenciesExchangeRate {
+  constructor(private readonly apiKey: string) {}
+
+  async getExchangeRate({
+    destinationCurrency,
+    originAmount,
+    originCurrency,
+  }: GetCurrenciesExchangeRateModel): Promise<ExchangeRateModel> {
+    try {
+      const response = await axios.get(
+        `https://api.apilayer.com/exchangerates_data/convert?to=${destinationCurrency}&from=${originAmount}&amount=${originCurrency}`,
+        {
+          headers: {
+            apikey: this.apiKey,
+          },
+        },
+      )
+
+      return {
+        rate: response.data.info.rate,
+        result: response.data.result,
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+}
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,6 @@ export default {
   jwtSecret: process.env.JWT_SECRET || 'fuadfhuadfhuad',
   databaseUrl: process.env.DATABASE_URL || 'file:./dev.db',
   enviroment: process.env.ENVIROMENT || 'development',
+  exchangeRatesApiKey:
+    process.env.EXCHANGERATESAPIKEY || 'pURjhI8X2712Rn914aliM267GdtUp8oL',
 }
