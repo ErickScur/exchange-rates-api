49aaad2ac53501545841adc41c721e0a790034c1
Erick Scur <erickscur@gmail.com>
2022-12-22 11:31:41 -0300

    feat: ensure DbVerifyToken calls LoadAccountByIdRepository with correct value

--- src/data/usecases/authentication/verify-token/db-verify-token.ts
+++ src/data/usecases/authentication/verify-token/db-verify-token.ts
@@ -1,12 +1,17 @@
 import { VerifyToken } from '../../../../domain/usecases/authentication/verify-token'
 import { Decrypter } from '../../../protocols/criptography/decrypter'
+import { LoadAccountByIdRepository } from '../../../protocols/db/account/load-account-by-id-repository'
 import { AccountModel } from '../add-account/db-add-account-protocols'
 
 export class DbVerifyToken implements VerifyToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+  ) {}
 
   async verify(token: string): Promise<AccountModel> {
-    await this.decrypter.decrypt(token)
+    const id = await this.decrypter.decrypt(token)
+    await this.loadAccountByIdRepository.loadById(id)
     return null
   }
 }
