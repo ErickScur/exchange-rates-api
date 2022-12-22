1085ff7cb29e6305ad018ccbea398842d3f57f4b
Erick Scur <erickscur@gmail.com>
2022-12-22 09:11:14 -0300

    test: ensure LogPrismaRepository creates an error log on success

---
+++ src/infra/db/prisma/repositories/log/log-prisma-repository.test.ts
@@ -0,0 +1,29 @@
+import { PrismaInstance } from '../../helpers/prisma-instance'
+import { LogPrismaRepository } from './log-prisma-repository'
+
+describe('Account Prisma Repository', () => {
+  const prismaInstance = new PrismaInstance()
+
+  const makeSut = (): LogPrismaRepository => {
+    return new LogPrismaRepository(prismaInstance)
+  }
+
+  beforeAll(async () => {
+    await prismaInstance.connect()
+  })
+
+  afterAll(async () => {
+    await prismaInstance.disconnect()
+  })
+
+  beforeEach(async () => {
+    await prismaInstance.account.deleteMany()
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError('any_error')
+    const count = await prismaInstance.log.count()
+    expect(count).toBe(1)
+  })
+})
