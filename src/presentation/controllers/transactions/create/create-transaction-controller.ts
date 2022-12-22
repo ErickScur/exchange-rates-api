ae3aa0f05b05dde79c7b2fe93703aa17a1ed8844
Erick Scur <erickscur@gmail.com>
2022-12-22 14:59:29 -0300

    feat: ensure CreateTransactionController calls AddTransaction with correct values

--- src/presentation/controllers/transactions/create/create-transaction-controller.ts
+++ src/presentation/controllers/transactions/create/create-transaction-controller.ts
@@ -1,3 +1,4 @@
+import { AddTransaction } from '../../../../domain/usecases/transactions/add-transaction'
 import { badRequest, unauthorized } from '../../../helpers/http-helper'
 import {
   Controller,
@@ -7,13 +8,26 @@ import {
 } from '../../../protocols'
 
 export class CreateTransactionController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addTransaction: AddTransaction,
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) return badRequest(error)
 
     if (!httpRequest.body.account) return unauthorized()
+
+    const { account, originCurrency, originAmount, destinationCurrency } =
+      httpRequest.body
+
+    await this.addTransaction.add({
+      accountId: account.id,
+      originCurrency,
+      originAmount,
+      destinationCurrency,
+    })
     return null
   }
 }
