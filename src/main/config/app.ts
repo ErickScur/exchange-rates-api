87525050d006d0ccb210e2c2696f12077770df9c
Erick Scur <erickscur@gmail.com>
2022-12-23 16:28:56 -0300

    feat: swagger api doc

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import swaggerUI from 'swagger-ui-express'
 
 const app = express()
 setupMiddlewares(app)
 setupRoutes(app)
+app.use(
+  '/api-docs',
+  swaggerUI.serve,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  swaggerUI.setup(require('./swagger.json')),
+)
 export default app
