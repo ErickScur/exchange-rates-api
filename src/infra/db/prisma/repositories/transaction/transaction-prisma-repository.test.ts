76cd32b7a5ee97e138265bf9c479a66e29b3b7a1
Erick Scur <erickscur@gmail.com>
2022-12-24 11:15:40 -0300

    test: ensure TransactionPrismaRepository returns a list of transactions on load success

--- src/infra/db/prisma/repositories/transaction/transaction-prisma-repository.test.ts
+++ src/infra/db/prisma/repositories/transaction/transaction-prisma-repository.test.ts
@@ -50,4 +50,47 @@ describe('Transaction Prisma Repository', () => {
     expect(transaction.originAmount).toBe(53)
     expect(transaction.originCurrency).toBe('BRL')
   })
+
+  test('Should return a transaction on add success', async () => {
+    const { id: accountId } = await prismaInstance.account.create({
+      data: {
+        email: 'any_email',
+        password: 'any_password',
+        name: 'any_name',
+      },
+    })
+
+    const sut = makeSut()
+    await sut.add({
+      accountId,
+      conversionRate: 10,
+      conversionRateLabel: '1 -> 5.3',
+      destinationAmount: 10,
+      destinationCurrency: 'USD',
+      originAmount: 53,
+      originCurrency: 'BRL',
+    })
+    await sut.add({
+      accountId,
+      conversionRate: 10,
+      conversionRateLabel: '1 -> 5.3',
+      destinationAmount: 10,
+      destinationCurrency: 'USD',
+      originAmount: 53,
+      originCurrency: 'BRL',
+    })
+
+    const transactions = await sut.load(accountId)
+
+    transactions.forEach((t) => {
+      expect(t).toBeTruthy()
+      expect(t.id).toBeTruthy()
+      expect(t.conversionRate).toBe(10)
+      expect(t.conversionRateLabel).toBe('1 -> 5.3')
+      expect(t.destinationAmount).toBe(10)
+      expect(t.destinationCurrency).toBe('USD')
+      expect(t.originAmount).toBe(53)
+      expect(t.originCurrency).toBe('BRL')
+    })
+  })
 })
